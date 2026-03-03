ce7077198eacd9f3794c0a348fd4cb544a5d74b2
Artur Bagautdinov <arturbagautdinov30@yahoo.com>
2026-03-03 11:08:09 +0300

    Initial Commit

---
+++ FitFlow/FitFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitFlowApp.swift
+//  FitFlow
+//
+//  Created by Artur Bagautdinov on 03.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct FitFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
